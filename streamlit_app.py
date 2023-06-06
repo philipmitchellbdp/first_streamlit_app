d0a86b138169ebb0d2ceb6eb8f7a24da0504c2cd
philipmitchellbdp <133992045+philipmitchellbdp@users.noreply.github.com>
2023-06-06 16:49:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,12 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
 # Display the table on the page.
 streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 # Normalise response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
